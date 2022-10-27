9721a63afc0f73598697f4e2dc24bfa4c4c19f96
David Kim <dkhw90@gmail.com>
2022-10-27 17:45:39 +0200

    Finish is palindrome linked list

--- easy/is_palindrome_linked_list.py
+++ easy/is_palindrome_linked_list.py
@@ -7,7 +7,13 @@ Output: boolean
 
 Example:
 
-sol.isPalindrome()
+1 - 2 - 2 - 1 # True
+1 - 2 # False
+
+Algorithm:
+
+- Traverse through the linked list and append each val to a list data structure
+- Compare the list to a reversed list of itself and return true if same
 
 """
 
@@ -31,7 +37,14 @@ def traverse_list_nodes(head):
 
 class Solution:
   def isPalindrome(self, head: Optional[ListNode]) -> bool:
-    return None
+    vals = []
+    curr_node = head
+    while curr_node:
+      vals.append(curr_node.val)
+      curr_node = curr_node.next
+    vals_reverse = vals[:]
+    vals_reverse.reverse()
+    return vals == vals_reverse
 
 
 head1 = ListNode(1)
@@ -40,5 +53,11 @@ head1.next.next = ListNode(2)
 head1.next.next.next = ListNode(1)
 traverse_list_nodes(head1)
 
-# sol = Solution()
-# print(sol.isPalindrome())
+sol = Solution()
+print(sol.isPalindrome(head1)) # expect True
+
+
+head2 = ListNode(1)
+head2.next = ListNode(2)
+sol = Solution()
+print(sol.isPalindrome(head2)) # expect False
