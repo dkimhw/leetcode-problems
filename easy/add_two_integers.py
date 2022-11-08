fd77fc3a4bce9d0dfb7686c56e6d0f6af7ca4606
David Kim <dkhw90@gmail.com>
2022-11-08 16:39:23 +0100

    Finish couple leetcode problems

---
+++ easy/add_two_integers.py
@@ -0,0 +1,21 @@
+
+"""
+
+Input: num1 (integer), num2 (integer)
+Output: num
+  - sum of the two input integers
+
+
+Input: num1 = 12, num2 = 5
+Output: 17
+Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+
+
+Algorithm:
+  - return Sum of num1 and num2
+
+"""
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+      return num1 + num2
