2b166c462cdaab6131b9d0703870478ccf190039
David Kim <dkhw90@gmail.com>
2022-12-01 18:40:06 +0100

    Finish pairs of songs prob

--- medium/pairs_of_songs_duration.py
+++ medium/pairs_of_songs_duration.py
@@ -53,10 +53,37 @@ Algorithm Attempt 2:
     - No this is combination formula: n! / (2! (n - 2)!)
 4. For index 0
   - 2 pairs = 1
-    - if it's
+
+
+Algorithm Attempt 3:
+1. Create a dictionary / Counter to track remainders
+2. Create a variable to keep track of total pairs - `results`
+3. Loop through `time`
+  - If duration % 60 == 0 then results += remainders[0] # meaning - if there was another pair of 60 in there - those can form pairs
+  - Else - we would want to check the corresponding index that can be paired to create x % 60 == 0
+    - (60 - duration) % 60
+    - You can pair the current time with all the instances in there
+  - Keep counting each t in `remainders`
+    - Don't want to do this at the top because it will overcount the pairs you have
 """
 from typing import List
+from collections import Counter
 
 class Solution:
   def numPairsDivisibleBy60(self, time: List[int]) -> int:
-    pass
+    remainders = Counter()
+    result = 0
+    for duration in time:
+      if duration % 60 == 0:
+        result += remainders[0]
+      else:
+        complementary_idx = (60 - duration) % 60
+        result += remainders[complementary_idx]
+      remainders[duration % 60] += 1
+    return result
+
+
+
+sol = Solution()
+# print(sol.numPairsDivisibleBy60([30,20,150,100,40])) # 3
+print(sol.numPairsDivisibleBy60([20,40])) # 1
