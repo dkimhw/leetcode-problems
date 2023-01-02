dc437a371d81870e25108d4a5d1e706757378155
David Kim <dkhw90@gmail.com>
2023-01-02 15:41:12 +0100

    Fix valid parentheses

--- easy/valid_parentheses.py
+++ easy/valid_parentheses.py
@@ -12,6 +12,8 @@ Output: true
 
 Input: s = "()[]{}"
 Output: true
+(, [, {
+), ], }
 
 Input: s = "(]"
 Output: false
@@ -22,15 +24,43 @@ Output: true
 Input: s = "(()[)"
 Output: false
 
-(,(,),[,)
+(,(,[
+), )
 
 Input: s = "(())"
 Output: false
 
-(, (, ), )
+(, (
+), )
 
-
+Algorithm:
+  - create parentheses stack
+  - loop through string
+    - if open parens - add to stack
+    - if close parens
+      - pop parenthese stack
+      - compare - make sure the open and closed are valid i.e. ()
+        - if not return false
+  - else retur true
 """
 
 class Solution:
-    def isValid(self, s: str) -> bool:
+  def isValid(self, s: str) -> bool:
+    parentheses = []
+    for char in s:
+      if char in ['(', '[', '{']:
+        parentheses.append(char)
+      else:
+        last_item = parentheses.pop()
+        if last_item == '(' and char != ')':
+          return False
+        elif last_item == '[' and char != ']':
+          return False
+        elif last_item == '{' and char != '}':
+          return False
+    return True
+
+sol = Solution()
+# print(sol.isValid("(())")) # true
+print(sol.isValid("(()[)")) # false
+print(sol.isValid("(()()[][()])")) # true
