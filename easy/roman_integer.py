372041ec4c0630b05406fdba6999f6ac2ddfae1f
David Kim <dkhw90@gmail.com>
2023-01-09 23:52:49 +0100

    Solve roman integer prob

--- easy/roman_integer.py
+++ easy/roman_integer.py
@@ -31,10 +31,49 @@ Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
 
-
-
+Algorithm:
+  - Create a map of roman char to int
+  - Create variable result = 0
+  - Loop through the roman numeral string
+    - if the next val is > than the current val
+      - take the next val and subtract it from current val
+      - move index forward by 2
+    - else
+      - take the val returned from map and add it to result
+      - move index forward by 1
 """
 
 
 class Solution:
-    def romanToInt(self, s: str) -> int:
+  def convertSingleRomanChar(self, s:str) -> int:
+    roman_to_int_val_map = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+    }
+    if s in roman_to_int_val_map:
+      return roman_to_int_val_map[s]
+    return None
+
+  def romanToInt(self, s: str) -> int:
+    result = 0
+    idx = 0
+    while idx < len(s):
+      curr_val = self.convertSingleRomanChar(s[idx])
+      next_val = self.convertSingleRomanChar(s[idx + 1]) if idx + 1 < len(s) else None
+
+      if next_val and next_val > curr_val:
+        result += next_val - curr_val
+        idx += 2
+      else:
+        result += curr_val
+        idx += 1
+    return result
+
+sol = Solution()
+print(sol.romanToInt('III'))
+print(sol.romanToInt("MCMXCIV"))
