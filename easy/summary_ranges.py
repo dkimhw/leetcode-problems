2e0d3fd24b75603919169cccdd08e739f6035445
David Kim <dkhw90@gmail.com>
2023-07-18 15:59:56 +0200

    Create a new more readable solution

--- easy/summary_ranges.py
+++ easy/summary_ranges.py
@@ -1,5 +1,6 @@
 
 """
+https://leetcode.com/problems/summary-ranges/editorial/
 
 Problem
 --------------
@@ -79,6 +80,25 @@ class Solution:
 
     return result
 
+
+class SolutionAlternate:
+  def summaryRanges(self, nums: List[int]) -> List[str]:
+    ranges = []
+    i = 0
+
+    while i < len(nums):
+        start = nums[i]
+        while i + 1 < len(nums) and nums[i] + 1 == nums[i + 1]:
+            i += 1
+
+        if start != nums[i]:
+            ranges.append(str(start) + "->" + str(nums[i]))
+        else:
+            ranges.append(str(nums[i]))
+
+        i += 1
+
+    return ranges
 
 sol = Solution()
 # print(sol.summaryRanges([0,1,2,4,5,7])) # ["0->2","4->5","7"]
